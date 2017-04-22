ed42c04ce43dd12b28e6dc172beac343c1391ec0
634750802 <634750802@qq.com>
2017-04-22 16:10:15 +0800

    修复公共成员列表显示问题

--- src/components/MemberList.vue
+++ src/components/MemberList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div id="member-list-root">
     <ul v-if="list != null" id="member-list">
       <li v-for="item in list">
         <img class="avatar" :src="item.avatar_url">
         <div class="content">
-          <p class="title">{{item.login}}</p>
-          <a class="url" :href="item.html_url">{{item.html_url}}</a>
+          <p class="title">{{item.name || item.login}}</p>
+          <a class="url" :href="item.html_url">{{item.login}}</a>
         </div>
       </li>
     </ul>
@@ -23,7 +23,21 @@ export default {
   mounted () {
     axios.get('https://api.github.com/orgs/neu-1923/members')
       .then(response => {
-        this.list = response.data
+        this.list = response.data.map(item => {
+          return {
+            name: null,
+            url: item.url,
+            html_url: item.html_url,
+            login: item.login,
+            avatar_url: item.avatar_url
+          }
+        })
+        this.list.forEach(member => {
+          axios.get(member.url)
+            .then(res => {
+              member.name = res.data.name
+            })
+        })
       })
   },
   data () {
@@ -36,10 +50,14 @@ export default {
 
 
 <style scoped>
-ul#member-list {
-  display: block;
+div#member-list-root {
   padding: 8px;
+}
+
+ul#member-list  {
+  display: block;
   margin: auto;
+  padding: 0;
   width: 400px;
   max-width: 100%;
 }
@@ -64,7 +82,6 @@ ul#member-list>li .avatar {
 }
 
 ul#member-list>li .content {
-  width: 350px;
   display: flex;
   flex-direction: column;
 }
